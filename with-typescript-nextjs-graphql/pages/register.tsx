05734fbb063bcd9f3badf6443e15032cc263d439
Dean <sixone0712@gmail.com>
2020-12-03 23:10:43 +0900

    5강까지 커밋

--- with-typescript-nextjs-graphql/pages/register.tsx
+++ with-typescript-nextjs-graphql/pages/register.tsx
@@ -2,13 +2,45 @@ import React from "react";
 import Layout from "../components/Layout";
 import { Formik, Field } from "formik";
 import { InputFiled } from "../components/fields/InputField";
+import {
+  useRegisterMutation,
+  RegisterInput,
+} from "../generated/apolloComponents";
 
 export default function Register() {
+  const [registerMutation, { data, loading, error }] = useRegisterMutation();
+
+  console.log("data", data);
+  console.log("loading", loading);
+  console.log("error", error?.message);
+
   return (
     <Layout title="Register page">
       <Formik
-        onSubmit={(data) => {
-          console.log("data", data);
+        validateOnBlur={false}
+        validateOnChange={false}
+        onSubmit={async (inputForm: RegisterInput, { setErrors }) => {
+          try {
+            const response = await registerMutation({
+              variables: {
+                data: inputForm,
+              },
+            });
+          } catch (e) {
+            console.error("error", Object.keys(e));
+            const errors: { [key: string]: string } = {};
+            e.graphQLErrors[0].extensions.exception.validationErrors.forEach(
+              (validationErr: any) => {
+                Object.values(validationErr.constraints).forEach(
+                  (message: any) => {
+                    errors[validationErr.property] = message;
+                  },
+                );
+              },
+            );
+            console.log("errors", errors);
+            setErrors(errors);
+          }
         }}
         initialValues={{
           email: "",
